8ab287188017e2ba04cb8940503afaf28e342e08
Dennis Liu <denliu@nvidia.com>
2025-05-11 19:40:00 -0700

    Add Torch mod tracker.

--- examples/mcore/run.sh
+++ examples/mcore/run.sh
@@ -7,13 +7,13 @@ export MEGATRON_PATH=/workspace/megatron-lm
 export PYTHONPATH=/workspace/pytorch-memory-tracing:$MEGATRON_PATH:$PYTHONPATH
 
 # Running for silicon
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-torchrun --nproc_per_node=8 --nnodes=1 pretrain_gpt.py --model moe --tp-size 2 --pp-size 1 --ep-size 1 --ga 8 
+# export CUDA_DEVICE_MAX_CONNECTIONS=1
+# torchrun --nproc_per_node=8 --nnodes=1 pretrain_gpt.py --model moe --tp-size 2 --pp-size 1 --ep-size 1 --ga 8 
 # > silicon.log 2>&1
 
 
 # Running for estimated
-# export WORLD_SIZE=8
-# export LOCAL_RANK=0
+export WORLD_SIZE=8
+export LOCAL_RANK=0
 
-# python pretrain_gpt.py --tp-size 2 --pp-size 2 --ep-size 2 --ga 8 --estimated --model moe > estimated.log 2>&1
+python pretrain_gpt.py --tp-size 2 --pp-size 2 --ep-size 2 --ga 8 --estimated --model moe 
